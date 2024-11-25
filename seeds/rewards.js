146b428aae5ab6ec55bf06bb90a546ed38270cba
nicolettecheng <nikocheng@gmail.com>
2024-11-25 12:59:09 -0500

    add array data

---
+++ seeds/rewards.js
@@ -0,0 +1,39 @@
+export default [
+  {
+    id: 1,
+    reward_name: "Brunch at Foundry Tavern",
+    reward_description:
+      "Book a brunch reso at Foundry Tavern for AYCE brunch $40/pp",
+    stars_required: 10,
+    user_id: 1,
+  },
+  {
+    id: 2,
+    reward_name: "Upgrade desk: buy macbook dock",
+    reward_description:
+      "Invest in a dock for my Macbook air to have a cleaner desk setup",
+    stars_required: 8,
+    user_id: 1,
+  },
+  {
+    id: 3,
+    reward_name: "Hotpot dinner",
+    reward_description: "Have a boujee hotpot dinner at home",
+    stars_required: 8,
+    user_id: 1,
+  },
+  {
+    id: 4,
+    reward_name: "Vancouver trip",
+    reward_description: "Go to Vancouver to visit Katie",
+    stars_required: 50,
+    user_id: 1,
+  },
+  {
+    id: 5,
+    reward_name: "Buy Hades 2",
+    reward_description: "Buy the Hades 2 game on steam",
+    stars_required: 15,
+    user_id: 1,
+  },
+];
---
+++ seeds/tasks.js
@@ -0,0 +1,36 @@
+export default [
+  {
+    id: 1,
+    task_name: "Pack for Europe",
+    task_descripotion:
+      "Pack check-in and carry-on suitcases for 1 month in Europe.",
+    stars: 5,
+  },
+  {
+    id: 2,
+    task_name: "Get feed topper for Pudding",
+    task_descripotion:
+      "Buy 2 diff types of food toppers for Pudding's meals, 1x fish and 1x chicken",
+    stars: 1,
+  },
+  {
+    id: 3,
+    task_name: "Do a Costco run",
+    task_descripotion: "Get souvenirs for Europe from Costco: veggie crackers",
+    stars: 2,
+  },
+  {
+    id: 4,
+    task_name: "Review flight info for Europe",
+    task_descripotion:
+      "Check if it's possible to choose seats and update renewed passport info",
+    stars: 1,
+  },
+  {
+    id: 5,
+    task_name: "Book Janurary nail appointment",
+    task_descripotion:
+      "Book nail appointment for around January 10th upon returning from EU",
+    stars: 1,
+  },
+];
