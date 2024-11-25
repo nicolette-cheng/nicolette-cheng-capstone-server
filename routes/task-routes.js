a15e53f44151a429102b36b857eb759a32daa415
nicolettecheng <nikocheng@gmail.com>
2024-11-25 15:05:06 -0500

    initialize task routes, import controller+express, add get all route

---
+++ routes/task-routes.js
@@ -0,0 +1,6 @@
+import * as taskController from "../controllers/task-controller.js";
+
+import express from "express";
+const router = express.Router();
+
+router.route("/").get(taskController.index);
