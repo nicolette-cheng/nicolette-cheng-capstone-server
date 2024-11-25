f22a63ea521aadebea39ea2d1b38fe7df581f6c0
nicolettecheng <nikocheng@gmail.com>
2024-11-25 15:22:13 -0500

    initialize controller and routes, add search func

---
+++ controllers/reward-controller.js
@@ -0,0 +1,31 @@
+import initKnex from "knex";
+import configuration from "../knexfile.js";
+const knex = initKnex(configuration);
+
+const index = async (req, res) => {
+  const { s } = req.query;
+
+  try {
+    const query = knex("rewards").select(
+      "id",
+      "reward_name",
+      "description",
+      "stars"
+    );
+
+    if (s) {
+      query.where(function () {
+        this.where("reward_name", "like", `%${s}%`)
+          .orWhere("description", "like", `%${s}%`)
+          .orWhere("stars", "like", `%${s}%`);
+      });
+    }
+
+    const data = await query;
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).send(`Error retrieving rewards: ${error}`);
+  }
+};
+
+export { index };
---
+++ routes/reward-routes.js
@@ -0,0 +1,6 @@
+import * as rewardController from "../controllers/reward-controller.js";
+
+import express from "express";
+const router = express.Router();
+
+router.route("/").get(rewardController.index);
