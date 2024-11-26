fda31e860cfbcf48bd6d6e984208a5b1c7cdde72
nicolettecheng <nikocheng@gmail.com>
2024-11-26 17:23:31 -0500

    add reward_id key value pair

--- seeds/tasks.js
+++ seeds/tasks.js
@@ -12,6 +12,7 @@ export async function seed(knex) {
       description:
         "Pack check-in and carry-on suitcases for 1 month in Europe.",
       stars_required: 5,
+      reward_id: 5,
       created_at: knex.fn.now(),
       updated_at: knex.fn.now(),
     },
@@ -21,6 +22,7 @@ export async function seed(knex) {
       description:
         "Buy 2 diff types of food toppers for Pudding's meals, 1x fish and 1x chicken",
       stars_required: 1,
+      reward_id: 1,
       created_at: knex.fn.now(),
       updated_at: knex.fn.now(),
     },
@@ -29,6 +31,7 @@ export async function seed(knex) {
       task_name: "Do a Costco run",
       description: "Get souvenirs for Europe from Costco: veggie crackers",
       stars_required: 2,
+      reward_id: 2,
       created_at: knex.fn.now(),
       updated_at: knex.fn.now(),
     },
@@ -38,15 +41,17 @@ export async function seed(knex) {
       description:
         "Check if it's possible to choose seats and update renewed passport info",
       stars_required: 1,
+      reward_id: 3,
       created_at: knex.fn.now(),
       updated_at: knex.fn.now(),
     },
     {
       id: 5,
-      task_name: "Book January nail appointment",
+      task_name: "Deploy wabisabi app",
       description:
-        "Book nail appointment for around January 10th upon returning from EU",
-      stars_required: 1,
+        "Deploy the wabisabi app on Heroku",
+      stars_required: 30,
+      reward_id: 4,
       created_at: knex.fn.now(),
       updated_at: knex.fn.now(),
     },
