5c4fe34abee5e3fa65d21fbc7dad52e7869ee6e5
nicolettecheng <nikocheng@gmail.com>
2024-11-29 19:45:00 -0500

    add route for getting single task

--- routes/task-routes.js
+++ routes/task-routes.js
@@ -4,5 +4,8 @@ import express from "express";
 const router = express.Router();
 
 router.route("/").get(taskController.index);
+// .post(taskController.createTaskItem);
+
+router.route("/:id").get(taskController.getSingleTask);
 
 export default router;
