2e8f4016a6faffb265526cfb2852bfb112458cc6
nicolettecheng <nikocheng@gmail.com>
2024-12-01 12:23:43 -0500

    add post route

--- routes/task-routes.js
+++ routes/task-routes.js
@@ -3,8 +3,7 @@ import * as taskController from "../controllers/task-controller.js";
 import express from "express";
 const router = express.Router();
 
-router.route("/").get(taskController.index);
-// .post(taskController.createTaskItem);
+router.route("/").get(taskController.index).post(taskController.createTaskItem);
 
 router.route("/:id").get(taskController.getSingleTask);
 
