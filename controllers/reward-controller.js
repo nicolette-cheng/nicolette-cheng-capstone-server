720d74bf3fb8c8bc472673804240a53be8d529ee
nicolettecheng <nikocheng@gmail.com>
2024-12-02 18:04:56 -0500

    add getSingleReward function

--- controllers/reward-controller.js
+++ controllers/reward-controller.js
@@ -28,4 +28,34 @@ const index = async (req, res) => {
   }
 };
 
-export { index };
+const getSingleReward = async (req, res) => {
+  try {
+    const rewardFound = await knex("rewards")
+      .select(
+        "id",
+        "reward_name",
+        "description",
+        "stars_required",
+        knex.raw("DATE(created_at) as created_at"),
+        knex.raw("DATE(updated_at) as updated_at")
+      )
+      .where({
+        id: req.params.id,
+      });
+
+    if (rewardFound.length === 0) {
+      return res.status(404).json({
+        message: `Reward with ID ${req.params.id} not found`,
+      });
+    }
+
+    const rewardData = rewardFound[0];
+    res.json(rewardData);
+  } catch (error) {
+    res.status(500).json({
+      message: `Unacle to retrieve reward data for task with ID ${req.params.id}`,
+    });
+  }
+};
+
+export { index, getSingleReward };
