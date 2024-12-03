bad37221ce7498b82016c707222ca70e3168fc57
nicolettecheng <nikocheng@gmail.com>
2024-12-03 14:55:28 -0500

    add post route

--- routes/reward-routes.js
+++ routes/reward-routes.js
@@ -3,7 +3,10 @@ import * as rewardController from "../controllers/reward-controller.js";
 import express from "express";
 const router = express.Router();
 
-router.route("/").get(rewardController.index);
+router
+  .route("/")
+  .get(rewardController.index)
+  .post(rewardController.createRewardItem);
 
 router.route("/:id").get(rewardController.getSingleReward);
 
