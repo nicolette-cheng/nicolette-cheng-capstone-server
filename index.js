8edefa6fd25d4772675b06fa9ca1c9724e6cc925
nicolettecheng <nikocheng@gmail.com>
2024-12-19 14:53:38 +0100

    add glimmerRoutes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import "dotenv/config";
 
 import taskRoutes from "./routes/task-routes.js";
 import rewardRoutes from "./routes/reward-routes.js";
+import glimmerRoutes from "./routes/glimmers-routes.js";
 
 const app = express();
 app.use(cors());
@@ -11,6 +12,7 @@ app.use(express.json());
 
 app.use("/tasks", taskRoutes);
 app.use("/rewards", rewardRoutes);
+app.use("/glimmers", glimmerRoutes);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
