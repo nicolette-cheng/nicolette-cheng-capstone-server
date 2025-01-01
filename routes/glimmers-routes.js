54a4233972015607fb16897df96c96adb69ddb88
nicolettecheng <nikocheng@gmail.com>
2025-01-01 17:12:50 +0100

    uncomment index get request route

--- routes/glimmers-routes.js
+++ routes/glimmers-routes.js
@@ -3,8 +3,9 @@ import * as glimmerController from "../controllers/glimmer-controller.js";
 import express from "express";
 const router = express.Router();
 
-router.route("/").get(glimmerController.index).post(glimmerController.createGlimmerItem);
+router.route("/").get(glimmerController.index)
+// .post(glimmerController.createGlimmerItem);
 
-router.route("/:id").get(glimmerController.getSingleGlimmer).put(glimmerController.editGlimmerItem);
+// router.route("/:id").get(glimmerController.getSingleGlimmer).put(glimmerController.editGlimmerItem);
 
 export default router;
